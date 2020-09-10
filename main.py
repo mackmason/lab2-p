b6debe21f5fa53862314f9503776266b1c87777a
Mack Mason <mackmason16@gmail.com>
2020-09-10 15:15:08 +0000

    new

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ def getLetterGrade(grade):
   grade = float(grade)
   if grade >= 93.0:
     return "A"
+    grade = "A"
   elif grade < 93.0 and grade >= 90.0:
     return "A-"
+    grade = "A-"
   elif grade < 90.0 and grade >= 87.0:
     return "B+"
+    grade = "B+"
   elif grade < 87.0 and grade >= 83.0:
     return "B"
+    grade = "B"
   elif grade < 83.0 and grade >= 80.0:
     return "B-"
+    grade = "B-"
   elif grade < 80.0 and grade >= 77.0:
     return "C+"
+    grade = "C+"
   elif grade < 77.0 and grade >= 70.0:
     return "C"
+    grade = "C"
   elif grade < 70.0 and grade >= 60.0:
     return "D"
+    grade = "D"
   else:
     return "F"
+    grade = "F"
  
 def run():
   grade = input("Enter your CMPSC 131 grade: ")
